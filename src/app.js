ea5aee6e111875a643feadd8d045c3355cc7c1fd
pabse <pabseabbate@gmail.com>
2024-03-20 22:15:42 -0300

    Backend---Abbate-Pablo

--- src/app.js
+++ src/app.js
@@ -3,8 +3,24 @@ import express from "express";
 import ProductRouter from "./router/product.routes.js";
 import CartRouter from "./router/carts.routes.js";
 
+import {engine} from "express-handlebars";
+import { __dirname } from "./utils.js";
+import * as path from "path"
+
+import {Server} from "socket.io";
+
 const app = express();
 const PORT = 8080;
+
+app.engine("handlebars", engine());
+app.set("view engine", "handlebars");
+app.set("views", path.resolve(__dirname + "/views"));
+
+app.use("/", express.static(__dirname + "/public"))
+
+app.get("/", (req, res) => {
+    res.render("home");
+});
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
@@ -12,6 +28,17 @@ app.use(express.urlencoded({extended: true}));
 app.use("/products", ProductRouter)
 app.use("/carts", CartRouter)
 
-app.listen(PORT, () => {
+const httpServer = app.listen(PORT, () => {
     console.log(`Servidor Express Puerto ${PORT}`);
+});
+
+
+const socketServer = new Server(httpServer);
+
+socketServer.on("connection", socket => {
+    console.log("Nuevo Cliente Conectado");
+
+    socket.on("message", data => {
+        console.log("Recibi el dato: ", data);
+    })
 });
---
+++ src/public/js/realTimeProducts.js
@@ -0,0 +1,4 @@
+
+const socket = io()
+
+socket.emit("message","hola me estoy comunicando desde un socket emit")
